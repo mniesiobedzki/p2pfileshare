efa8191af880ff556ca5c15d06ecdeed38f39b02
Samael_PL <marcin.samael.weiss@gmail.com>
2013-04-27 18:09:07 +0200

    commit
    commmmmmmmit

--- P2Pnode/src/folder/FolderTree.java
+++ P2Pnode/src/folder/FolderTree.java
@@ -57,9 +57,8 @@ public class FolderTree {
 	}
 	
 	public void updateAll(){
-		for(LinkedList<FileState> f: File.filesAndTheirHistory.values()){
-			
-			if(f.getLast()
+		for(File f: File.files.values()){
+			if(f.getSingleFileHistory().getLast()
 		}
 	}
 	
