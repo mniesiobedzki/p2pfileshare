dd4177014f87c931fc9f870034a831e14a1dea26
Samael_PL <marcin.samael.weiss@gmail.com>
2013-04-27 13:48:53 +0200

    commit
    commit

--- P2Pnode/src/FolderTree.java
+++ P2Pnode/src/FolderTree.java
@@ -4,11 +4,11 @@ import pl.edu.pjwstk.mteam.jcsync.core.implementation.collections.JCSyncTreeMap;
 public class FolderTree {
 	public static JCSyncTreeMap<String, Nod> folder = new JCSyncTreeMap<String, Nod>();
 	public static JCSyncTreeMap<String, Nod> folderOld = new JCSyncTreeMap<String, Nod>();
-	public static JCS
+	public static Nod root;
 public FolderTree(String path){
-	
+	root=new Nod(path, null);
 }
-public JCSyncTreeMap<String, String> getFolder(){
+public JCSyncTreeMap<String, Nod> getFolder(){
 	return folder;
 }
 	
