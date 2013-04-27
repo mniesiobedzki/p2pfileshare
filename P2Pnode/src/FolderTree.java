5fa7e6892822fa42024d856238585511a7dd7181
Samael_PL <marcin.samael.weiss@gmail.com>
2013-04-27 13:39:41 +0200

    mega comit
    ciągle mega comit

--- P2Pnode/src/FolderTree.java
+++ P2Pnode/src/FolderTree.java
@@ -1,4 +1,15 @@
+import pl.edu.pjwstk.mteam.jcsync.core.implementation.collections.JCSyncTreeMap;
+
 
 public class FolderTree {
-
+	public static JCSyncTreeMap<String, Nod> folder = new JCSyncTreeMap<String, Nod>();
+	public static JCSyncTreeMap<String, Nod> folderOld = new JCSyncTreeMap<String, Nod>();
+	public static JCS
+public FolderTree(String path){
+	
+}
+public JCSyncTreeMap<String, String> getFolder(){
+	return folder;
+}
+	
 }
---
+++ P2Pnode/src/Nod.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+
+public class Nod {
+	String value;
+	LinkedList<Nod> children= new LinkedList<Nod>();
+	Nod parent;
+	
+	public Nod(String val, Nod par){
+		value=val;
+		parent=par;
+	}
+	public void addChlid(Nod ch){
+		if(!children.contains(ch)){
+			children.add(ch);
+		}
+	}
+	public void removeChild(Nod ch){
+		if(!children.contains(ch)){
+			children.remove(ch);
+		}
+	}
+}
