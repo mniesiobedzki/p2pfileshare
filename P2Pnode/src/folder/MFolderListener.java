07af8a7ec970cfe54a5fbb4b1a8d540dd65146e4
StarWars <proprogramista@gmail.com>
2013-05-14 21:06:56 +0200

    pelny sync

--- P2Pnode/src/folder/MFolderListener.java
+++ P2Pnode/src/folder/MFolderListener.java
@@ -12,8 +12,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.objenesis.instantiator.basic.NewInstanceInstantiator;
-
 import file.File;
 
 public class MFolderListener {
