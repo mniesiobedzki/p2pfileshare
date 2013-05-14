4a92e0057484ddb0f454dc952c6ac6da26a8657f
Samael_PL <marcin.samael.weiss@gmail.com>
2013-05-14 21:27:09 +0200

    commit

--- P2Pnode/src/test/TestTreeServer.java
+++ P2Pnode/src/test/TestTreeServer.java
@@ -6,17 +6,19 @@ import file.File;
 import folder.FolderTree;
 import folder.MFolderListener;
 
-public class TestTreeServer implements Runnable{
+public class TestTreeServer implements Runnable {
 
 	/***
-	 * @param fname - folder name
-	 * @param uname - user name
-	 * @param path - file path
+	 * @param fname
+	 *            - folder name
+	 * @param uname
+	 *            - user name
+	 * @param path
+	 *            - file path
 	 */
 	String fname;
 	String uname;
 	String path;
-	
 
 	@Override
 	public void run() {
@@ -24,25 +26,29 @@ public class TestTreeServer implements Runnable{
 		try {
 			welcomeSocket = new ServerSocket(6789);
 
-		FolderTree ft = new FolderTree(fname);
-		ft.addUser(uname, path);
-		// File fa = new File(name, File.generateFileId(uname));
-		java.io.File[] listaFajli = File.listAllTheFilesInDir(path);
+			FolderTree ft = new FolderTree(fname);
+			ft.addUser(uname, path);
+			// File fa = new File(name, File.generateFileId(uname));
+			java.io.File[] listaFajli = File.listAllTheFilesInDir(path);
 
-		for (java.io.File file : listaFajli) {
-			File f = new File(file.getName(), uname);
-			ft.addFile(f, uname);
-			MFolderListener.filesAndTheirHistory.put(uname + f.getFileName(), f);
-			System.out.println(f.getFileId());
-		}
-		MFolderListener.runFolderListener(path, ft, uname);
-		Socket connectionSocket = welcomeSocket.accept();
-		System.err.println("Connected");
+			for (java.io.File file : listaFajli) {
+				File f = new File(file.getName(), uname);
+				ft.addFile(f, uname);
+				MFolderListener.filesAndTheirHistory.put(
+						uname + f.getFileName(), f);
+				System.out.println(f.getFileId());
+			}
+			MFolderListener.runFolderListener(path, ft, uname);
+			Socket connectionSocket = welcomeSocket.accept();
+			System.err.println("Connected");
 
-		OutputStream socketStream = connectionSocket.getOutputStream();
-		ObjectOutput objectOutput = new ObjectOutputStream(socketStream);
-		objectOutput.writeObject(ft);
-
+			OutputStream socketStream = connectionSocket.getOutputStream();
+			ObjectOutput objectOutput = new ObjectOutputStream(socketStream);
+			while (true) {
+				if (MFolderListener.fileCreated || MFolderListener.fileDeleted || MFolderListener.fileModified) {
+					objectOutput.writeObject(ft);
+				}
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
