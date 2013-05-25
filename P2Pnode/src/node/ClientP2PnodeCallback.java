cae07fa87860ad99c6f4c0f6249c3ce5bc678cba
mniesiobedzki <marek@niesiobedzki.pl>
2013-05-25 15:46:18 +0200

    porapwki bledow

--- P2Pnode/src/node/ClientP2PnodeCallback.java
+++ P2Pnode/src/node/ClientP2PnodeCallback.java
@@ -8,28 +8,28 @@ import java.util.List;
 
 /**
  * Callback-i
- * @author marek niesiobędzki
  *
+ * @author marek niesiobędzki
  */
 public class ClientP2PnodeCallback implements NodeCallback {
 
-	@Override
-	public boolean onDeliverRequest(List<NetworkObject> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean onDeliverRequest(List<NetworkObject> arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public void onDisconnect(Node arg0) {
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " disconnected");
-		
-	}
+    @Override
+    public void onDisconnect(Node arg0) {
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " disconnected");
 
-	@Override
-	public boolean onForwardingRequest(List<NetworkObject> arg0) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    }
+
+    @Override
+    public boolean onForwardingRequest(List<NetworkObject> arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
     @Override
     public void onBootstrapError(Node node, int i) {
@@ -41,110 +41,111 @@ public class ClientP2PnodeCallback implements NodeCallback {
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
+
     @Override
-	public void onInsertObject(Node arg0, NetworkObject arg1) {
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " inserts new object");
-		
-	}
+    public void onInsertObject(Node arg0, NetworkObject arg1) {
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " inserts new object");
 
-	@Override
-	public void onJoin(Node arg0) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " connected");
-		
-	}
+    }
 
-	@Override
-	public void onObjectLookup(Node arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onObjectLookup.");
-		
-	}
+    @Override
+    public void onJoin(Node arg0) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " connected");
 
-	@Override
-	public void onOverlayError(Node arg0, Object arg1, int arg2) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onOverlayError.");
-		
-	}
+    }
 
-	@Override
-	public void onOverlayError(Node arg0, Object arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onOverlayError.");
-		
-	}
+    @Override
+    public void onObjectLookup(Node arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onObjectLookup.");
 
-	@Override
-	public void onPubSubError(Node arg0, Object arg1, short arg2, int arg3) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onPubSubError.");
-		
-	}
+    }
 
-	@Override
-	public void onPubSubError(Node arg0, Object arg1, short arg2, int arg3,
-			int arg4) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onPubSubError.");
-		
-	}
+    @Override
+    public void onOverlayError(Node arg0, Object arg1, int arg2) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onOverlayError.");
 
-	@Override
-	public void onTopicCreate(Node arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicCreate.");
-		
-	}
+    }
 
-	@Override
-	public void onTopicCreate(Node arg0, Object arg1, int arg2) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicCreate.");
-		
-	}
+    @Override
+    public void onOverlayError(Node arg0, Object arg1, int arg2, int arg3) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onOverlayError.");
 
-	@Override
-	public void onTopicNotify(Node arg0, Object arg1, byte[] arg2,
-			boolean arg3, short arg4) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicNotify.");
-		
-	}
+    }
 
-	@Override
-	public void onTopicRemove(Node arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicRemove.");
-		
-	}
+    @Override
+    public void onPubSubError(Node arg0, Object arg1, short arg2, int arg3) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onPubSubError.");
 
-	@Override
-	public void onTopicSubscribe(Node arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicSubscribe.");
-		
-	}
+    }
 
-	@Override
-	public void onTopicSubscribe(Node arg0, Object arg1, int arg2) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicSubscribe.");
-		
-	}
+    @Override
+    public void onPubSubError(Node arg0, Object arg1, short arg2, int arg3,
+                              int arg4) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onPubSubError.");
 
-	@Override
-	public void onTopicUnsubscribe(Node arg0, Object arg1, int arg2) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicUnsubscribe.");
-		
-	}
+    }
 
-	@Override
-	public void onUserLookup(Node arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onUserLookup.");
-		
-	}
+    @Override
+    public void onTopicCreate(Node arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicCreate.");
+
+    }
+
+    @Override
+    public void onTopicCreate(Node arg0, Object arg1, int arg2) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicCreate.");
+
+    }
+
+    @Override
+    public void onTopicNotify(Node arg0, Object arg1, byte[] arg2,
+                              boolean arg3, short arg4) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicNotify.");
+
+    }
+
+    @Override
+    public void onTopicRemove(Node arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicRemove.");
+
+    }
+
+    @Override
+    public void onTopicSubscribe(Node arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicSubscribe.");
+
+    }
+
+    @Override
+    public void onTopicSubscribe(Node arg0, Object arg1, int arg2) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicSubscribe.");
+
+    }
+
+    @Override
+    public void onTopicUnsubscribe(Node arg0, Object arg1, int arg2) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onTopicUnsubscribe.");
+
+    }
+
+    @Override
+    public void onUserLookup(Node arg0, Object arg1) {
+        // TODO Auto-generated method stub
+        System.out.println("NodeCallback: Node " + arg0.getUserName() + " -> onUserLookup.");
+
+    }
 
 }
