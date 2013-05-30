701e8a79c0ca0f96092b2f7bb8c5aef3a7198f8b
Samael_PL <marcin.samael.weiss@gmail.com>
2013-05-30 22:10:02 +0200

    commit

--- P2Pnode/src/folder/FolderTree.java
+++ P2Pnode/src/folder/FolderTree.java
@@ -266,6 +266,8 @@ public class FolderTree implements Serializable {
                 FileClient fileClient = new FileClient(this, nod, folder2.get(nod.getParent()).ip, nod.getParent() + nod.getName(), folder.get(usr).getPath(), nod.getName(), usr, folder.get(nod.getParent()).port);
             } else {
                 //kod kasujący plik
+            	System.out.println("Kasuje plik: "+ nod.name);
+            	System.out.println("Ścieżka: "+ folder.get(usr).getPath() + nod.getName());
                 MFolderListener.deleteFileFromDisc(folder.get(usr).getPath() + nod.getName());
                 if (this.getFolder().containsKey(usr + nod.getName())) {
                     this.getFolder().get(usr + nod.getName()).history.add(null);
