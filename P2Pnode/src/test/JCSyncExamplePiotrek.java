0b14cab34a2275be3df3240e101fe7d2d6cb5a5d
mniesiobedzki <marek@niesiobedzki.pl>
2013-06-11 20:45:05 +0200

    Testowanie JCSyncExample z paczki ups - porawka - test powtarzalny

--- P2Pnode/src/test/JCSyncExamplePiotrek.java
+++ P2Pnode/src/test/JCSyncExamplePiotrek.java
@@ -113,7 +113,6 @@ public class JCSyncExamplePiotrek {
             //To change body of implemented methods use File | Settings | File Templates.
         }
 
-
     };
 
     private JCSyncCore jcsyncCore;
